c876084f4ec6fa2cd1f1cec1f102d376b2412792
vansh-000 <23165@iiitu.ac.in>
2026-01-23 02:26:54 +0530

    feat: add student retrieval endpoints for current and previous courses

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -7,6 +7,8 @@ import {
     findFacultiesByPrevCourseAndBatch,
     findFacultyByCourseId,
     findFacultyByPrevCourseId,
+    findStudentByCourseId,
+    findStudentByPrevCourseId,
     getCourseById,
     getCourseByInstitution,
     getCoursesByDepartment,
@@ -21,10 +23,12 @@ const router = Router();
 router.get("/department/:departmentId", getCoursesByDepartment);
 router.get("/institution/:institutionId", getCourseByInstitution);
 router.get("/:courseId", getCourseById);
-router.get("/faculty/course/:courseId", findFacultyByCourseId);
-router.get("/faculty/prev-course/:courseId", findFacultyByPrevCourseId);
-router.get("/faculty/course/:courseId/batch/:batch", findFacultiesByCourseAndBatch);
-router.get("/faculty/prev-course/:courseId/batch/:batch", findFacultiesByPrevCourseAndBatch);
+router.get("/faculty/course/:courseId/institution/:institutionId", findFacultyByCourseId);
+router.get("/faculty/prev-course/:courseId/institution/:institutionId", findFacultyByPrevCourseId);
+router.get("/faculty/course/:courseId/institution/:institutionId/batch/:batch", findFacultiesByCourseAndBatch);
+router.get("/faculty/prev-course/:courseId/institution/:institutionId/batch/:batch", findFacultiesByPrevCourseAndBatch);
+router.get("student/course/:courseId/institution/:institutionId", findStudentByCourseId);
+router.get("student/prev-course/:courseId/institution/:institutionId", findStudentByPrevCourseId);
 
 // Protected Routes
 router.post("/create-course", validateInstitutionJWT, createCourse);
